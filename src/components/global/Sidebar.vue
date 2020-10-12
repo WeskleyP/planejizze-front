d5fb56e33ceb94f56fa38560d49bd4da92dfd19e
WeskleyP <weskleypedro@gmail.com>
2020-10-12 12:52:52 -0300

    Iniciando layout básico

---
+++ src/components/global/Sidebar.vue
@@ -0,0 +1,99 @@
+<template>
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+        :mini-variant.sync="mini"
+        permanent
+        color="menuBg"
+    >
+        <v-list class="pa-0" min-height="56">
+            <v-list-item
+                @click.stop="mini = !mini"
+                class="bg-menus principalTitle lh"
+            >
+                <v-list-item-icon>
+                    <v-icon class="white--text">mdi-alpha-p </v-icon>
+                </v-list-item-icon>
+                <v-list-item-title class="white--text font-weight-bold size"
+                    >Planejizze</v-list-item-title
+                >
+            </v-list-item>
+        </v-list>
+        <v-list color="pa-0">
+            <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                link
+                class="hovering ma-1 pl-3 menus bg-menus"
+            >
+                <v-list-item-icon>
+                    <v-icon left size="24px" class="white--text text-center">{{
+                        item.icon
+                    }}</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                    <v-list-item-title class="white--text text-left">{{
+                        item.title
+                    }}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+        <template v-slot:append>
+            <v-list dense>
+                <v-list-item class="hovering ma-1 pl-3 menus bg-menus">
+                    <v-list-item-icon>
+                        <v-icon size="24px" class="white--text text-center"
+                            >mdi-crown</v-icon
+                        >
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text text-left"
+                            >Seja Premium</v-list-item-title
+                        >
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </template>
+    </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            drawer: true,
+            items: [
+                { title: "Dashboard", icon: "mdi-view-dashboard" },
+                { title: "Receitas", icon: "mdi-currency-usd" },
+                { title: "Despesas", icon: "mdi-briefcase" },
+                { title: "Planejamento", icon: "mdi-lightbulb-outline" },
+                { title: "Administrador", icon: "mdi-domain" },
+                { title: "Relatórios", icon: "mdi-chart-timeline-variant" }
+            ],
+            mini: true
+        };
+    }
+};
+</script>
+
+<style>
+.hovering:hover {
+    background: #700049;
+}
+.size {
+    font-size: 28px;
+    font-family: Share Tech, sans-serif;
+}
+.menus {
+    font-family: Open Sans, sans-serif;
+    font-size: 14px;
+}
+.bg-menus {
+    background: rgba(255, 255, 255, 0.05);
+}
+.lh {
+    height: 56px;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,21 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#5D069A",
+                secondary: "#5A029A",
+                bgColor: "#EFEFEF",
+                menuBg: "#310055",
+                hoverColor: "#700049",
+                premium: "#C39300",
+                bgIndex: "#F2F2F2",
+                dolarBg: "#5D069A4D",
+                graphColorBg: "#41062dA6",
+                principalTitle: "#4E107B"
+            }
+        }
+    }
+});
