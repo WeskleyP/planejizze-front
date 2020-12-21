4e5c1d7795505c9a815abf5b0a7fac2fc4d4598c
WeskleyP <weskleypedro@gmail.com>
2020-12-21 11:11:43 -0300

    Removendo chartjs e adicionando apexcharts

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -3,24 +3,44 @@
         <v-row>
             <v-col cols="6" lg="6" sm="12">
                 <v-card elevation="4">
-                    <pie-chart :chart-data="pieData" />
+                    <apexchart
+                        width="380"
+                        type="pie"
+                        :options="pieData.chartOptions"
+                        :series="pieData.series"
+                    ></apexchart>
                 </v-card>
             </v-col>
             <v-col cols="6" lg="6" sm="12">
                 <v-card elevation="4">
-                    <horizontal-bar-chart :chart-data="horizontalBarData" />
+                    <apexchart
+                        width="380"
+                        type="bar"
+                        :options="horizontalBarData.chartOptions"
+                        :series="horizontalBarData.series"
+                    ></apexchart>
                 </v-card>
             </v-col>
         </v-row>
         <v-row>
             <v-col cols="6" lg="6" sm="12">
                 <v-card elevation="4">
-                    <bar-chart :chart-data="barData" />
+                    <apexchart
+                        width="380"
+                        type="bar"
+                        :options="barData.chartOptions"
+                        :series="barData.series"
+                    ></apexchart>
                 </v-card>
             </v-col>
             <v-col cols="6" lg="6" sm="12">
                 <v-card elevation="4">
-                    <scatter-chart :chart-data="scatterData" />
+                    <apexchart
+                        width="380"
+                        type="scatter"
+                        :options="scatterData.chartOptions"
+                        :series="scatterData.series"
+                    ></apexchart>
                 </v-card>
             </v-col>
         </v-row>
@@ -28,18 +48,7 @@
 </template>
 
 <script>
-import PieChart from "../components/graphs/PieChart";
-import BarChart from "../components/graphs/BarChart";
-import HorizontalBarChart from "../components/graphs/HorizontalBarChart";
-import ScatterChart from "../components/graphs/ScatterChart";
-
 export default {
-    components: {
-        PieChart,
-        BarChart,
-        HorizontalBarChart,
-        ScatterChart
-    },
     data() {
         return {
             pieData: null,
@@ -57,81 +66,259 @@ export default {
     methods: {
         fillPieData() {
             this.pieData = {
-                labels: [this.getRandomInt(), this.getRandomInt()],
-                datasets: [
-                    {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
+                series: [44, 55, 13, 43, 22],
+                chartOptions: {
+                    chart: {
+                        width: 380,
+                        type: "pie"
                     },
-                    {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
-                    }
-                ]
+                    labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+                    responsive: [
+                        {
+                            breakpoint: 480,
+                            options: {
+                                chart: {
+                                    width: 200
+                                },
+                                legend: {
+                                    position: "bottom"
+                                }
+                            }
+                        }
+                    ]
+                }
             };
         },
         fillHorizontalBarData() {
             this.horizontalBarData = {
-                labels: [this.getRandomInt(), this.getRandomInt()],
-                datasets: [
+                series: [
                     {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
-                    },
-                    {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
+                        data: [
+                            400,
+                            430,
+                            448,
+                            470,
+                            540,
+                            580,
+                            690,
+                            1100,
+                            1200,
+                            1380
+                        ]
                     }
-                ]
+                ],
+                chartOptions: {
+                    chart: {
+                        type: "bar",
+                        height: 350
+                    },
+                    plotOptions: {
+                        bar: {
+                            horizontal: true
+                        }
+                    },
+                    dataLabels: {
+                        enabled: false
+                    },
+                    xaxis: {
+                        categories: [
+                            "South Korea",
+                            "Canada",
+                            "United Kingdom",
+                            "Netherlands",
+                            "Italy",
+                            "France",
+                            "Japan",
+                            "United States",
+                            "China",
+                            "Germany"
+                        ]
+                    }
+                }
             };
             console.log("montando");
         },
         fillBarData() {
             this.barData = {
-                labels: [this.getRandomInt(), this.getRandomInt()],
-                datasets: [
+                series: [
                     {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
-                    },
-                    {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
+                        data: [
+                            400,
+                            430,
+                            448,
+                            470,
+                            540,
+                            580,
+                            690,
+                            1100,
+                            1200,
+                            1380
+                        ]
                     }
-                ]
+                ],
+                chartOptions: {
+                    chart: {
+                        type: "bar",
+                        height: 350
+                    },
+                    plotOptions: {
+                        bar: {
+                            horizontal: false
+                        }
+                    },
+                    dataLabels: {
+                        enabled: false
+                    },
+                    xaxis: {
+                        categories: [
+                            "South Korea",
+                            "Canada",
+                            "United Kingdom",
+                            "Netherlands",
+                            "Italy",
+                            "France",
+                            "Japan",
+                            "United States",
+                            "China",
+                            "Germany"
+                        ]
+                    }
+                }
             };
         },
         fillScatterData() {
             this.scatterData = {
-                labels: [this.getRandomInt(), this.getRandomInt()],
-                datasets: [
+                series: [
                     {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
+                        name: "SAMPLE A",
+                        data: [
+                            [16.4, 5.4],
+                            [21.7, 2],
+                            [25.4, 3],
+                            [19, 2],
+                            [10.9, 1],
+                            [13.6, 3.2],
+                            [10.9, 7.4],
+                            [10.9, 0],
+                            [10.9, 8.2],
+                            [16.4, 0],
+                            [16.4, 1.8],
+                            [13.6, 0.3],
+                            [13.6, 0],
+                            [29.9, 0],
+                            [27.1, 2.3],
+                            [16.4, 0],
+                            [13.6, 3.7],
+                            [10.9, 5.2],
+                            [16.4, 6.5],
+                            [10.9, 0],
+                            [24.5, 7.1],
+                            [10.9, 0],
+                            [8.1, 4.7],
+                            [19, 0],
+                            [21.7, 1.8],
+                            [27.1, 0],
+                            [24.5, 0],
+                            [27.1, 0],
+                            [29.9, 1.5],
+                            [27.1, 0.8],
+                            [22.1, 2]
+                        ]
                     },
                     {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
+                        name: "SAMPLE B",
+                        data: [
+                            [36.4, 13.4],
+                            [1.7, 11],
+                            [5.4, 8],
+                            [9, 17],
+                            [1.9, 4],
+                            [3.6, 12.2],
+                            [1.9, 14.4],
+                            [1.9, 9],
+                            [1.9, 13.2],
+                            [1.4, 7],
+                            [6.4, 8.8],
+                            [3.6, 4.3],
+                            [1.6, 10],
+                            [9.9, 2],
+                            [7.1, 15],
+                            [1.4, 0],
+                            [3.6, 13.7],
+                            [1.9, 15.2],
+                            [6.4, 16.5],
+                            [0.9, 10],
+                            [4.5, 17.1],
+                            [10.9, 10],
+                            [0.1, 14.7],
+                            [9, 10],
+                            [12.7, 11.8],
+                            [2.1, 10],
+                            [2.5, 10],
+                            [27.1, 10],
+                            [2.9, 11.5],
+                            [7.1, 10.8],
+                            [2.1, 12]
+                        ]
                     },
                     {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
-                    },
-                    {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
+                        name: "SAMPLE C",
+                        data: [
+                            [21.7, 3],
+                            [23.6, 3.5],
+                            [24.6, 3],
+                            [29.9, 3],
+                            [21.7, 20],
+                            [23, 2],
+                            [10.9, 3],
+                            [28, 4],
+                            [27.1, 0.3],
+                            [16.4, 4],
+                            [13.6, 0],
+                            [19, 5],
+                            [22.4, 3],
+                            [24.5, 3],
+                            [32.6, 3],
+                            [27.1, 4],
+                            [29.6, 6],
+                            [31.6, 8],
+                            [21.6, 5],
+                            [20.9, 4],
+                            [22.4, 0],
+                            [32.6, 10.3],
+                            [29.7, 20.8],
+                            [24.5, 0.8],
+                            [21.4, 0],
+                            [21.7, 6.9],
+                            [28.6, 7.7],
+                            [15.4, 0],
+                            [18.1, 0],
+                            [33.4, 0],
+                            [16.4, 0]
+                        ]
                     }
-                ]
+                ],
+                chartOptions: {
+                    chart: {
+                        height: 350,
+                        type: "scatter",
+                        zoom: {
+                            enabled: true,
+                            type: "xy"
+                        }
+                    },
+                    xaxis: {
+                        tickAmount: 10,
+                        labels: {
+                            formatter: function(val) {
+                                return parseFloat(val).toFixed(1);
+                            }
+                        }
+                    },
+                    yaxis: {
+                        tickAmount: 7
+                    }
+                }
             };
         },
         getRandomInt() {
--- src/views/Receitas.vue
+++ src/views/Receitas.vue
@@ -32,6 +32,7 @@
                         :items="desserts"
                         :items-per-page="5"
                         class="elevation-1 fw"
+                        no-data-text="Não foi encontrado nenhum dado!"
                     ></v-data-table>
                 </v-row>
             </v-col>
@@ -45,18 +46,22 @@
             >
                 <v-row justify="center">
                     <v-card elevation="4">
-                        <pie-chart
-                            :chart-data="pieData"
-                            :options="pieOptions"
-                        />
+                        <apexchart
+                            width="380"
+                            type="line"
+                            :options="options"
+                            :series="series"
+                        ></apexchart>
                     </v-card>
                 </v-row>
                 <v-row justify="center">
                     <v-card elevation="4">
-                        <horizontal-bar-chart
-                            :chart-data="pieData"
-                            :options="pieOptions"
-                        />
+                        <apexchart
+                            width="380"
+                            type="pie"
+                            :options="options"
+                            :series="series"
+                        ></apexchart>
                     </v-card>
                 </v-row>
             </v-col>
@@ -64,114 +69,29 @@
     </v-container>
 </template>
 <script>
-import PieChart from "../components/graphs/PieChart";
-import HorizontalBarChart from "../components/graphs/HorizontalBarChart";
 export default {
-    components: {
-        PieChart,
-        HorizontalBarChart
-    },
     data() {
         return {
+            options: {},
+            series: [44, 55, 41, 17, 15],
             pieData: null,
             pieOptions: {
                 responsive: true
             },
             headers: [
                 {
-                    text: "Dessert (100g serving)",
+                    text: "Descrição",
                     align: "start",
                     sortable: false,
-                    value: "name"
+                    value: "descricao"
                 },
-                { text: "Calories", value: "calories" },
-                { text: "Fat (g)", value: "fat" },
-                { text: "Carbs (g)", value: "carbs" },
-                { text: "Protein (g)", value: "protein" },
-                { text: "Iron (%)", value: "iron" }
+                { text: "Categoria", value: "categoria" },
+                { text: "Valor", value: "valor" },
+                { text: "Recebimento", value: "dataRecebimento" },
+                { text: "Status", value: "status" },
+                { text: "Repete-se?", value: "repetir" }
             ],
-            desserts: [
-                {
-                    name: "Frozen Yogurt",
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                    iron: "1%"
-                },
-                {
-                    name: "Ice cream sandwich",
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                    iron: "1%"
-                },
-                {
-                    name: "Eclair",
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                    iron: "7%"
-                },
-                {
-                    name: "Cupcake",
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                    iron: "8%"
-                },
-                {
-                    name: "Gingerbread",
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                    iron: "16%"
-                },
-                {
-                    name: "Jelly bean",
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                    iron: "0%"
-                },
-                {
-                    name: "Lollipop",
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                    iron: "2%"
-                },
-                {
-                    name: "Honeycomb",
-                    calories: 408,
-                    fat: 3.2,
-                    carbs: 87,
-                    protein: 6.5,
-                    iron: "45%"
-                },
-                {
-                    name: "Donut",
-                    calories: 452,
-                    fat: 25.0,
-                    carbs: 51,
-                    protein: 4.9,
-                    iron: "22%"
-                },
-                {
-                    name: "KitKat",
-                    calories: 518,
-                    fat: 26.0,
-                    carbs: 65,
-                    protein: 7,
-                    iron: "6%"
-                }
-            ]
+            desserts: []
         };
     },
     mounted() {
