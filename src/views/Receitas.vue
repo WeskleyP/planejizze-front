6f8df0836b1f843063522773de481447ba44bdee
WeskleyP <weskleypedro@gmail.com>
2020-12-21 21:41:32 -0300

    Finalizando layout tela receita

--- src/views/Receitas.vue
+++ src/views/Receitas.vue
@@ -1,8 +1,8 @@
 <template>
     <v-container fluid class="fill-height">
         <v-row>
-            <v-col cols="8" sm="12" md="6" lg="8" class="ma-5">
-                <v-row class="py-4">
+            <v-col cols="8" sm="12" md="6" lg="8" class="my-5">
+                <v-row class="pb-4">
                     <v-item-group class="bgColor align">
                         <v-card
                             elevation="2"
@@ -27,38 +27,79 @@
                     </v-item-group>
                 </v-row>
                 <v-row class="my-4">
+                    <v-sheet
+                        class="px-4 pt-4 pb-3 text-center my-5"
+                        tile
+                        width="100%"
+                    >
+                        <div class="text-h2">Suas Receitas</div>
+                    </v-sheet>
                     <v-data-table
                         :headers="headers"
-                        :items="desserts"
+                        :items="receitas"
                         :items-per-page="5"
                         class="elevation-1 fw"
                         no-data-text="Não foi encontrado nenhum dado!"
-                    ></v-data-table>
+                    >
+                        <template v-slot:top>
+                            <v-toolbar flat>
+                                <v-text-field
+                                    v-model="search"
+                                    append-icon="mdi-magnify"
+                                    label="Pesquisar"
+                                    single-line
+                                    hide-details
+                                ></v-text-field>
+                                <v-btn color="primary" class="mb-2 ml-2">
+                                    Adicionar nova receita
+                                </v-btn>
+                            </v-toolbar>
+                        </template>
+                        <template v-slot:item.actions="{ item }">
+                            <v-icon small class="mr-2" @click="editItem(item)">
+                                mdi-pencil
+                            </v-icon>
+                            <v-icon small @click="deleteItem(item)">
+                                mdi-delete
+                            </v-icon>
+                        </template>
+                    </v-data-table>
                 </v-row>
             </v-col>
-            <v-col
-                cols="3"
-                sm="12"
-                md="5"
-                lg="3"
-                class="ml-5 jc"
-                align-self="center"
-            >
-                <v-row justify="center">
-                    <v-card elevation="4">
+            <v-col cols="4" sm="12" md="5" lg="4" align-self="center">
+                <v-row justify="center" class="mb-3">
+                    <v-card elevation="4" class="bgColor" width="95%">
+                        <v-sheet
+                            class="px-4 pt-4 pb-3 text-center bgColor"
+                            tile
+                            width="100%"
+                        >
+                            <div class="text-h6">
+                                Receitas nos últimos 6 meses
+                            </div>
+                        </v-sheet>
                         <apexchart
-                            width="380"
-                            type="line"
-                            :options="options"
-                            :series="series"
+                            width="100%"
+                            type="bar"
+                            :options="optionsBar"
+                            :series="lineSeries"
                         ></apexchart>
                     </v-card>
                 </v-row>
-                <v-row justify="center">
-                    <v-card elevation="4">
+                <v-spacer />
+                <v-row justify="center" class="my-4">
+                    <v-card elevation="4" class="bgColor" width="95%">
+                        <v-sheet
+                            class="px-4 pt-4 pb-3 text-center bgColor"
+                            tile
+                            width="100%"
+                        >
+                            <div class="text-h6">Receitas por categoria</div>
+                        </v-sheet>
                         <apexchart
-                            width="380"
+                            width="100%"
                             type="pie"
+                            height="600"
                             :options="options"
                             :series="series"
                         ></apexchart>
@@ -72,8 +113,29 @@
 export default {
     data() {
         return {
+            search: "",
             options: {},
+            optionsBar: {
+                chart: {
+                    toolbar: {
+                        show: false
+                    }
+                },
+                plotOptions: {
+                    bar: {
+                        horizontal: true
+                    }
+                },
+                dataLabels: {
+                    enabled: false
+                }
+            },
             series: [44, 55, 41, 17, 15],
+            lineSeries: [
+                {
+                    data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
+                }
+            ],
             pieData: null,
             pieOptions: {
                 responsive: true
@@ -89,32 +151,42 @@ export default {
                 { text: "Valor", value: "valor" },
                 { text: "Recebimento", value: "dataRecebimento" },
                 { text: "Status", value: "status" },
-                { text: "Repete-se?", value: "repetir" }
+                { text: "Repete-se?", value: "repetir" },
+                { text: "Opções", value: "actions" }
             ],
-            desserts: []
+            receitas: [
+                {
+                    descricao: "TEste desc",
+                    categoria: "Teste",
+                    valor: "5000",
+                    dataRecebimento: "20/12/2020",
+                    status: "A PAGAR",
+                    repetir: "Não"
+                },
+                {
+                    descricao: "TEste desc",
+                    categoria: "Teste",
+                    valor: "5000",
+                    dataRecebimento: "20/12/2020",
+                    status: "A PAGAR",
+                    repetir: "Não"
+                },
+                {
+                    descricao: "TEste desc",
+                    categoria: "Teste",
+                    valor: "5000",
+                    dataRecebimento: "20/12/2020",
+                    status: "A PAGAR",
+                    repetir: "Não"
+                }
+            ]
         };
     },
     mounted() {
         this.fillPieData();
     },
     methods: {
-        fillPieData() {
-            this.pieData = {
-                labels: [this.getRandomInt(), this.getRandomInt()],
-                datasets: [
-                    {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
-                    },
-                    {
-                        label: "Data One",
-                        backgroundColor: "#f87979",
-                        data: [this.getRandomInt(), this.getRandomInt()]
-                    }
-                ]
-            };
-        },
+        fillPieData() {},
         getRandomInt() {
             return Math.floor(Math.random() * (50 - 5 + 1)) + 5;
         }
